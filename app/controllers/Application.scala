54d25e45bad151ff83ff3426ed43ee8c833e6c7c
Corey Auger <coreyauger@gmail.com>
2014-09-28 22:51:56 -0700

    added token to routes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -24,7 +24,7 @@ object Application extends Controller {
   }
 
   // don't forget to secure this...
-  def api(uuid: String) = WebSocket.async[JsValue] { request =>
+  def api(uuid: String, token:String) = WebSocket.async[JsValue] { request =>
     println(s"call to api: $uuid")
     def returnAuthExcetion = {
       println("Auth Exception")
@@ -32,7 +32,7 @@ object Application extends Controller {
       val in = Iteratee.foreach[JsValue] { event =>}
       // Send a single 'Hello!' message and close
       val out = Enumerator(Json.obj("op" -> "exception", "slot" -> "exception", "msg" -> "Not Authorized").asInstanceOf[JsValue]) >>> Enumerator.eof
-      future {
+      Future {
         (in, out)
       }
     }
@@ -60,7 +60,7 @@ object Application extends Controller {
 
   // NOTE: there are some issues with comet http chunking to be aware of..
   // See my blog post on the matter => http://affinetechnology.blogspot.ca/2014/03/play-framework-comet-chunking-support.html
-  def comet(uuid: String) = Action{
+  def comet(uuid: String, token:String = "") = Action{
     val enumerator = Await.result(WebSocketHandler.connectComet(uuid), 3.seconds)
     Comet.CometMessage
     // TODO: comet user actors never die .. since there is no disconnect message that is sent
